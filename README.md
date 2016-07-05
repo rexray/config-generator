d70dc830d230751066ca69ce37e65f0b7b80a697
Kendrick Coleman <kendrickcoleman@gmail.com>
2016-07-05 12:24:29 -0400

    initial release
    this initial release marks v0.1.0. More to follow with items listed in the To-Do section of the README
    
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

---
+++ README.md
@@ -0,0 +1,31 @@
+# REX-Ray Configuration Generator
+
+[http://rexrayconfig.emccode.com](http://rexrayconfig.emccode.com)
+
+## Description
+A dynamically create [REX-Ray configurations](http://rexray.readthedocs.io/en/latest/user-guide/config/) based upon form inputs.
+
+## Usage Instructions
+Choose the service to add and begin filling out the forms. Angular.js will detect changes and automatically build out your `config.yml` used for REX-Ray. 
+
+At this time, only 1 service can be added and it's encouraged to validate using [yamllint](http://www.yamllint.com/). 
+
+## Future
+
+- Add new service types as they are introduced in v0.4.X. 
+  - New service forms are added using the `/services` folder
+- Integrate pre-error checks for certain services where one value takes precedent over another. 
+   - example. ScaleIO `System Name` vs `System ID`.
+- Add Modal/Tooltip/Popover for individual labels explaining what each means.
+  - This will cut down on the need to refer back to the documentation.
+- Add ability to have multiple services and utilize modules
+- Add libStorage Server configuration options
+
+## Contribution
+Create a fork of the project into your own reposity. Make all your necessary changes and create a pull request with a description on what was added or removed and details explaining the changes in lines of code. If approved, project owners will merge it
+
+## Licensing
+rexray-config-generator is freely distributed under the [MIT License](http://github.com/kacole2/rexray-config-generator/LICENSE "LICENSE"). See LICENSE for details.
+
+## Support
+Please file bugs and issues on the Github issues page for this project. This is to help keep track and document everything related to this repo. For general discussions and further support you can join the [EMC {code} Community slack channel](http://community.emccode.com/). The code and documentation are released with no warranties or SLAs and are intended to be supported through a community driven process.
---
+++ js/formdata.js
@@ -0,0 +1,120 @@
+var mainForm = angular.module('mainForm', ['ui.bootstrap']);
+
+mainForm.controller('GlobalLoggingLevelController', ['$scope', function($scope){
+	$scope.loggingLevelsData = {
+  		loggingLevelOptions: [
+			{value: 'error', label: 'Error'},
+			{value: 'warn', label: 'Warn'},
+			{value: 'info', label: 'Info'},
+			{value: 'debug', label: 'Debug'}
+		],
+		selectedLoggingLevel: {value: 'warn', label: 'Warn'}
+  	 };
+}]);
+
+mainForm.controller('AddServiceController', ['$scope', function($scope){
+	$scope.serviceData = {
+		serviceRepeatSelect: null,
+  		serviceOptions: [
+			{value: 'scaleio', label: 'EMC ScaleIO'},
+			{value: 'isilon', label: 'EMC Isilon'},
+			{value: 'virtualbox', label: 'Oracle VirtualBox'}
+		]
+  	 };
+}]);
+
+mainForm.controller('AddServiceButtonController', ['$scope', '$rootScope', function($scope, $rootScope){
+	  	$rootScope.services = [];
+  
+		$scope.addNewService = function() {
+			if ($scope.services.length >= 1){
+				alert('Only 1 Service Supported At This Time.');
+			} else {
+				var serviceType = $scope.$parent.serviceData.serviceRepeatSelect
+				if ( serviceType !== null) {
+					var newServiceNo = $scope.services.length+1;
+					if (serviceType == 'scaleio'){
+						$scope.services.push({
+							"id":newServiceNo,
+							"type": serviceType,
+							"apiVersion": "2.0",
+							"insecure" : true,
+							"thinOrThick" : "ThinProvisioned"
+						});
+					} else if (serviceType == 'isilon'){
+						$scope.services.push({
+							"id":newServiceNo,
+							"type": serviceType
+						});
+					} else if (serviceType == 'virtualbox'){
+						$scope.services.push({
+							"id":newServiceNo,
+							"type": serviceType,
+							"endpoint": "http://127.0.0.1:18083",
+							"tls": false,
+							"volumePath": "$HOME/VirtualBox/Volumes",
+							"controllerName": "SATA"
+						});
+					} 
+				}
+			}
+		};
+		
+		/* NOT USED YET
+		$scope.removeService = function() {
+			var lastItem = $scope.choices.length-1;
+			$scope.choices.splice(lastItem);
+		};
+		*/
+}]);
+
+mainForm.filter('kvFilter', function(){
+	return function (objects){
+		var allKeys = []
+		angular.forEach(objects, function (value, key) {
+				allKeys.push(key);
+            })
+		return allKeys
+}})
+
+mainForm.filter('multiStringFilter', function(){
+	return function (input, searchText, AND_OR) {
+        var returnArray = [],
+            // Split on single or multi space
+            splitext = searchText.toLowerCase().split(/\s+/),
+            // Build Regexp with Logical AND using "look ahead assertions"
+            regexp_and = "(?=.*" + splitext.join(")(?=.*") + ")",
+            // Build Regexp with logicial OR
+            regexp_or = searchText.toLowerCase().replace(/\s+/g, "|"),
+            // Compile the regular expression
+            re = new RegExp((AND_OR == "AND") ? regexp_and : regexp_or, "i");
+
+        for (var x = 0; x < input.length; x++) {
+            if (re.test(input[x])) returnArray.push(input[x]);
+        }
+        /* View what the 2 regular expression look like
+        console.log(regexp_or);
+        console.log(regexp_and);*/
+        return returnArray;
+    }
+})
+
+mainForm.directive('yamlConfiguration', function(){
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		 scope: true, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'AE',
+		//template: 'My services: {{services}}<br>My Logging Level: {{loggingLevelsData.selectedLoggingLevel.value}} </br>',
+		 templateUrl: 'output/yaml.html',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($rootScope, $scope, iElm, iAttrs, controller) {
+			//console.log($rootScope);
+		}
+	};
+});
